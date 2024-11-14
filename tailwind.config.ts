9dc56df9480306e22d5c96b68b43db8ffbaba28d
Andre <andremicael@gmail.com>
2024-11-14 10:33:49 -0400

    style: adicionado estrutua de páginas configurações tailwind de tela e fonte

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        screens: {
+            xxs: '393px',
+            xs: '480px',
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+            '4xl': '1920px',
+        },
+        fontFamily: {
+            encode: ['Encode Sans', 'sans-serif'],
+            encodeCondensed: ['Encode Sans Condensed', 'sans-serif'],
+            encodeSemiCondensed: ['Encode Sans Semi Condensed', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                background: 'var(--background)',
+                foreground: 'var(--foreground)',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
